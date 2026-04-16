0d473f1ba1d224f5b82774e74250fb9aec52d9ac
SayaliBandvalkar <bandvalkarsayali27@gmail.com>
2026-04-17 02:03:36 +0530

    Fix PostgreSQL database configuration for Render

--- fedsecure/settings.py
+++ fedsecure/settings.py
@@ -78,13 +78,28 @@ WSGI_APPLICATION = 'fedsecure.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASE_URL = os.environ.get("DATABASE_URL")
+
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=DATABASE_URL
+#     )
+# }
+
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=DATABASE_URL
-    )
-}
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
