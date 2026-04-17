dea836eef8ed5c4dbb1a3caf83c96c6b9a4ff0d5
SayaliBandvalkar <bandvalkarsayali27@gmail.com>
2026-04-17 21:20:31 +0530

    fix user creation

--- core/apps.py
+++ core/apps.py
@@ -26,23 +26,56 @@
 
 
 
+# from django.apps import AppConfig
+# import os
+
+# class CoreConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'core'
+
+#     def ready(self):
+#         # Prevent duplicate execution
+#         if os.environ.get('RUN_MAIN') != 'true':
+#             return
+
+#         # Keep your existing signal
+#         from .signals import connect_training_signal
+#         connect_training_signal()
+
+#         # ✅ Create default demo user
+#         from django.contrib.auth.models import User
+
+#         if not User.objects.filter(username='analyst').exists():
+#             User.objects.create_user(
+#                 username='analyst',
+#                 password='analyst123'
+#             )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from django.apps import AppConfig
-import os
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
     def ready(self):
-        # Prevent duplicate execution
-        if os.environ.get('RUN_MAIN') != 'true':
-            return
-
-        # Keep your existing signal
+        # your signal
         from .signals import connect_training_signal
         connect_training_signal()
 
-        # ✅ Create default demo user
+        # create user
         from django.contrib.auth.models import User
 
         if not User.objects.filter(username='analyst').exists():
